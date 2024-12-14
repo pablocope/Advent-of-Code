323ab70622cb55a6ad71b822c0e7c4d2c58ab20e
Pablo C P <pcobper@upv.edu.es>
2024-12-14 16:59:13 +0100

    Implementado el código de la parte 2

--- Dia_1/Dia_1.cpp
+++ Dia_1/Dia_1.cpp
@@ -84,6 +84,61 @@ int distancia_listas(const vector<int>& l1, const vector<int>& l2){
     return suma;
 }
 
+//Función de búsqueda binaria para la parte dos
+int BusquedaBinaria(int x, vector<int> v, int ini, int fin){
+
+	if(ini > fin){
+		return -1; //No se encuentra en el array
+	}
+
+	int medio = (ini + fin)/2;
+
+	if(v[medio] == x){
+		return medio;
+	}else if(v[medio] > x){
+		return BusquedaBinaria(x, v, ini, medio-1);
+	}else{
+		return BusquedaBinaria(x, v, medio+1, fin);
+	}
+}
+
+//Función para encontrar los números iguales de una lista
+int cantidad_similar(int x, vector<int> v){
+	int similares = 0;
+	int pos_x = BusquedaBinaria(x, v, 0, v.size());
+
+	if(pos_x == -1){
+		return 0;
+	}
+
+	similares++;
+
+	int pos_x_right = pos_x + 1;
+	int pos_x_left = pos_x - 1;
+
+	while(x = v[pos_x_left]){ //miramos las posiciones anteriores a la que hemos encontrado
+		pos_x_left--;
+		similares++;
+	}
+	while(x = v[pos_x_right]){ //miramos las posiciones anteriores a la que hemos encontrado
+		pos_x_right++;
+		similares++;
+	}
+
+	return similares;
+}
+
+int calculo_similares(vector<int> l1, vector<int> l2){
+	int similares = 0;
+
+	for(int i = 0; i < l1.size(); i++){
+		cout << l1[i]*cantidad_similar(l1[i], l2) << " + ";
+		similares = similares + l1[i]*cantidad_similar(l1[i], l2);
+	}
+
+	return similares;
+}
+
 //-----------------------------------------------------------------------------------
 //                              PROGRAMA PRINCIPAL
 //-----------------------------------------------------------------------------------
@@ -110,6 +165,8 @@ int main(){
     imprimir_lista(lista_2);
 
     cout << "La distancia entre las listas es " << distancia_listas(lista_1, lista_2) << endl;
+
+	cout << "SEGUNDA PARTE" << endl << "El cálculo del similarity score es:" << calculo_similares(lista_1, lista_2);
 
     return 0;
 }
