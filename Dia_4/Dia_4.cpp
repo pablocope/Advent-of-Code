5e790401eb716a66f36e76b4481dbba7f5323662
Pablo C P <pcobper@upv.edu.es>
2024-12-23 21:37:40 +0100

    Corregidos los errores, primera estrella conseguida

--- Dia_4/Dia_4.cpp
+++ Dia_4/Dia_4.cpp
@@ -27,18 +27,102 @@ void leer_lista(vector<vector<char>>& matriz){
     }
 }
 
-int main(){
-
-    vector<vector<char>> sopa_letras;
-
-    leer_lista(sopa_letras);
-
+void imprimir_sopa(vector<vector<char>> sopa_letras){
     for(int i = 0; i < sopa_letras.size(); i++){
         for(int j = 0; j < sopa_letras[i].size(); j++){
             cout << sopa_letras[i][j];
         }
         cout << endl;
     }
+}
+
+bool verificar_rango(vector<vector<char>>& sopa, int i, int j) {
+    return i >= 0 && i < sopa.size() && j >= 0 && j < sopa[i].size();
+}
+
+int contar_xmas(vector<vector<char>>& sopa_letras){
+    int total = 0;
+
+    for(int i = 0; i < sopa_letras.size(); i++){
+        for(int j = 0; j < sopa_letras[i].size(); j++){
+            if(sopa_letras[i][j] == 'X'){
+                //Comprobamos hacia la derecha
+                // Comprobamos hacia la derecha
+                if (verificar_rango(sopa_letras, i, j + 3) &&
+                    sopa_letras[i][j + 1] == 'M' &&
+                    sopa_letras[i][j + 2] == 'A' &&
+                    sopa_letras[i][j + 3] == 'S') {
+                    total++;
+                }
+                // Comprobamos hacia la izquierda
+                if (verificar_rango(sopa_letras, i, j - 3) &&
+                    sopa_letras[i][j - 1] == 'M' &&
+                    sopa_letras[i][j - 2] == 'A' &&
+                    sopa_letras[i][j - 3] == 'S') {
+                    total++;
+                }
+                // Comprobamos hacia abajo
+                if (verificar_rango(sopa_letras, i + 3, j) &&
+                    sopa_letras[i + 1][j] == 'M' &&
+                    sopa_letras[i + 2][j] == 'A' &&
+                    sopa_letras[i + 3][j] == 'S') {
+                    total++;
+                }
+                // Comprobamos hacia arriba
+                if (verificar_rango(sopa_letras, i - 3, j) &&
+                    sopa_letras[i - 1][j] == 'M' &&
+                    sopa_letras[i - 2][j] == 'A' &&
+                    sopa_letras[i - 3][j] == 'S') {
+                    total++;
+                }
+                // Comprobamos diagonal arriba derecha
+                if (verificar_rango(sopa_letras, i - 3, j + 3) &&
+                    sopa_letras[i - 1][j + 1] == 'M' &&
+                    sopa_letras[i - 2][j + 2] == 'A' &&
+                    sopa_letras[i - 3][j + 3] == 'S') {
+                    total++;
+                }
+                // Comprobamos diagonal abajo derecha
+                if (verificar_rango(sopa_letras, i + 3, j + 3) &&
+                    sopa_letras[i + 1][j + 1] == 'M' &&
+                    sopa_letras[i + 2][j + 2] == 'A' &&
+                    sopa_letras[i + 3][j + 3] == 'S') {
+                    total++;
+                }
+                // Comprobamos diagonal abajo izquierda
+                if (verificar_rango(sopa_letras, i + 3, j - 3) &&
+                    sopa_letras[i + 1][j - 1] == 'M' &&
+                    sopa_letras[i + 2][j - 2] == 'A' &&
+                    sopa_letras[i + 3][j - 3] == 'S') {
+                    total++;
+                }
+                // Comprobamos diagonal arriba izquierda
+                if (verificar_rango(sopa_letras, i - 3, j - 3) &&
+                    sopa_letras[i - 1][j - 1] == 'M' &&
+                    sopa_letras[i - 2][j - 2] == 'A' &&
+                    sopa_letras[i - 3][j - 3] == 'S') {
+                    total++;
+                }
+            }
+        }
+    }
+
+    return total;
+}
+
+int main(){
+
+    vector<vector<char>> sopa_letras;
+
+    leer_lista(sopa_letras);
+
+    //imprimir_sopa(sopa_letras);
+
+    int contador = 0;
+    
+    contador =  contar_xmas(sopa_letras);
+    
+    cout << contador;
 
     return 0;
 }
