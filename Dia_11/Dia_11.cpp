e59907512e03603c08a042574282e0ea0e035146
Pablo C P <pcobper@upv.edu.es>
2025-01-04 13:50:59 +0100

    He cambiado de usar vectores a listas debido a que hay que hacer muchas inserciones. Ahora mismo el código parece que funciona y los valores del ejemplo los reproduce correctamente, sin embargo a la hora de aplicarlo con el input el AoC no devuelve el valor correcto.

--- Dia_11/Dia_11.cpp
+++ Dia_11/Dia_11.cpp
@@ -3,11 +3,11 @@
 #include <cmath>
 #include <sstream>
 #include <string>
-#include <vector>
+#include <list>
 
 using namespace std;
 
-void leer_lista(vector<int>& v){
+void leer_lista(list<int>& lista){
     string input;
     cout << "Introduce la lista de piedras:" << endl;
 
@@ -16,24 +16,63 @@ void leer_lista(vector<int>& v){
     int numero;
         
     while(line_stream >> numero){
-        v.push_back(numero);
+        lista.push_back(numero);
     }
     
 }
 
-void print(vector<int> v){
-    for(int i = 0; i < v.size(); i++){
-        cout << v[i] << endl;
+void print(list<int> lista){
+    list<int>::iterator it = lista.begin(); //creamos un iterador para que apunte a un elemento de la lista
+    
+    for (it = lista.begin(); it != lista.end(); it++) {
+        cout << *it << " ";
     }
     cout << endl;
 }
 
+int contarCifras(int numero) {
+    if(numero == 0){
+        return 1;
+    }
+
+    int cifras = 0;
+    while(numero > 0){
+        numero /= 10;
+        cifras++;
+    }
+    return cifras;
+}
+
+int blink(list<int>& stones, int n){
+    if(n <= 0){
+        return stones.size();
+    }
+
+    list<int>::iterator it = stones.begin();
+    for(it; it != stones.end(); advance(it, 1)){
+        if(*it == 0){
+            *it = 1;
+        }else if(contarCifras(*it) % 2 == 0){
+            int mitad_sup = (*it / pow(10, contarCifras(*it)/2));
+            int mitad_inf = *it - (mitad_sup * pow(10, contarCifras(*it)/2));
+            *it = mitad_inf;
+            stones.insert(it, mitad_sup);
+        }else{
+            *it *= 2024;
+        }
+    }
+    print(stones);
+    return blink(stones, n - 1);
+}
+
 int main(){
 
-    vector<int> stones;
+    list<int> stones;
 
     leer_lista(stones);
-    print(stones);
+    //print(stones);
+
+    cout << blink(stones, 25) << endl;
 
     return 0;
 }
